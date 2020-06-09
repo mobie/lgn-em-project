051103dfda66f4abb06ca9771d713fa710ad52f5
Constantin Pape <c.pape@gmx.net>
2020-06-09 23:34:05 +0200

    Add initialization script WIP

---
+++ initialize_mobie.py
@@ -0,0 +1,16 @@
+import argparse
+import mobie
+
+
+# TODO initialize the dataset from the knossos file
+def initialize_mobie():
+    pass
+
+
+# TODO add the mask dataset
+def add_mask():
+    pass
+
+
+if __name__ == '__main__':
+    pass
