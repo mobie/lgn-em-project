3b62c497d041153719aee676266ac771152d496a
Constantin Pape <c.pape@gmx.net>
2020-06-11 13:08:01 +0200

    Update size histogram calculation

--- bouton-segmentation/size_histogram.py
+++ bouton-segmentation/size_histogram.py
@@ -15,39 +15,48 @@ def _load_sizes(table_path):
     return sizes
 
 
-def to_volume(sizes, resolution=[]):
+def to_radius(sizes, resolution=.02):
     return sizes
+    radius = (.75 * sizes / np.pi) ** (1. / 3)
+    return radius
 
 
-def compute_size_histogram(table_path, out_path,
+def compute_size_histogram(table_path, out_path=None,
                            lower_percentile=1, upper_percentile=99):
     sizes = _load_sizes(table_path)
     thresh_low = np.percentile(sizes, lower_percentile)
     thresh_high = np.percentile(sizes, upper_percentile)
 
+    print("Initial number of objects:", len(sizes))
     sizes = sizes[sizes > thresh_low]
     sizes = sizes[sizes < thresh_high]
+    print("After thresholds:", len(sizes))
 
-    volumes = to_volume(sizes)
+    radii = to_radius(sizes)
 
     fig, ax = plt.subplots(3)
-    _, bins, _ = ax[0].hist(volumes)
-    ax[0].xlabel = 'bouton-volume [cubic microns]'
+    _, bins, _ = ax[0].hist(radii, bins=32)
+    ax[0].set_title("Full Size Histogram")
 
     size_range = bins[1]
-    volumes_small = volumes[volumes < size_range]
-    volumes_large = volumes[volumes > size_range]
+    radii_small = radii[radii < size_range]
+    radii_large = radii[radii > size_range]
 
-    ax[1].hist(volumes_small)
-    ax[1].xlabel = 'bouton-volume [cubic microns]'
+    ax[1].hist(radii_small, bins=32)
+    ax[1].set_title("Histogram of the first bin")
 
-    ax[2].hist(volumes_large)
-    ax[2].xlabel = 'bouton-volume [cubic microns]'
+    ax[2].hist(radii_large, bins=32)
+    ax[2].set_title("Histogram of the other bins")
+    ax[2].set_xlabel('radius [micron meter]')
 
-    plt.savefig(out_path)
+    if out_path is None:
+        plt.show()
+    else:
+        plt.savefig(out_path)
 
 
 if __name__ == '__main__':
     table_path = '../data/0.0.0/tables/sbem-adult-1-lgn-boutons/default.csv'
     out_path = '../size_histogram.png'
+    out_path = None
     compute_size_histogram(table_path, out_path)
