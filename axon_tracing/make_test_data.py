37fac49801b0af7e531378dae9ea4cb00fc6ba49
Constantin Pape <constantin.pape@embl.de>
2020-12-04 11:37:31 +0100

    Use different center point

--- axon_tracing/make_test_data.py
+++ axon_tracing/make_test_data.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import h5py
 import z5py
 
@@ -11,6 +12,8 @@ PATH = '/g/rompani/lgn-em-datasets/data/0.0.0/images/local/sbem-adult-1-lgn-raw.
 BOUTON_PATH = '/g/rompani/lgn-em-datasets/data/0.0.0/images/local/sbem-adult-1-lgn-boutons.n5'
 MODEL_PATH = os.path.join('/g/kreshuk/pape/Work/my_projects/super_embeddings/experiments/lgn',
                           'proofread/z100/imws_saved_state.torch')
+
+CENTER = (338.21969826291144, 115.66693588888701, 33.773648522038826)
 
 
 def run_prediction(raw):
@@ -38,31 +41,38 @@ def run_prediction(raw):
     pred = predict_with_halo(raw, ckpt, gpus=gpus, inner_block_shape=block_shape,
                              outer_block_shape=block_shape, preprocess=preprocess,
                              postprocess=postprocess)
+    # TODO the proper way would be to also mirror affinities according to their offset
+    # to boundaries
+    pred = np.max(pred[:4], axis=0)
+    pred = normalize_input(pred)
     return pred
 
 
-def make_test_data(halo=[50, 512, 512]):
+def make_test_data(halo=[64, 512, 512]):
+    resolution = [.04, .01, .01]
     with z5py.File(PATH, 'r') as f:
         ds = f['setup0/timepoint0/s0']
-        shape = ds.shape
 
-        bb = tuple(slice(sh // 2 - ha, sh // 2 + ha) for sh, ha in zip(shape, halo))
+        center = [int(ce / res) for ce, res in zip(CENTER[::-1], resolution)]
+        bb = tuple(slice(ce - ha, ce + ha) for ce, ha in zip(center, halo))
         raw = ds[bb]
 
     pred = run_prediction(raw)
 
     with h5py.File('./test_data.h5', 'a') as f:
         f.create_dataset('raw', data=raw, compression='gzip')
-        f.create_dataset('affinities', data=pred, compression='gzip')
+        f.create_dataset('boundaries', data=pred, compression='gzip')
 
 
-def add_boutons(scale_factor=[1, 2, 2], halo=[50, 512, 512]):
+def add_boutons(scale_factor=[1, 2, 2], halo=[64, 512, 512]):
+    resolution = [.04, .02, .02]
     with z5py.File(BOUTON_PATH, 'r') as f:
         ds = f['setup0/timepoint0/s0']
         shape = ds.shape
 
-        bb = tuple(slice(sh // 2 - ha // sf, sh // 2 + ha // sf)
-                   for sh, ha, sf in zip(shape, halo, scale_factor))
+        center = [int(ce / res) for ce, res in zip(CENTER[::-1], resolution)]
+        bb = tuple(slice(ce - ha // sf, ce + ha // sf)
+                   for ce, ha, sf in zip(center, halo, scale_factor))
         seg = ds[bb]
 
     target_shape = tuple(sh * sf for sh, sf in zip(seg.shape, scale_factor))
@@ -73,5 +83,5 @@ def add_boutons(scale_factor=[1, 2, 2], halo=[50, 512, 512]):
 
 
 if __name__ == '__main__':
-    # make_test_data()
+    make_test_data()
     add_boutons()
