9f9f110540a9132822ac54525c77351fec808a9c
Constantin Pape <constantin.pape@embl.de>
2020-12-18 11:16:10 +0100

    Add s3 dummy data

--- add_to_s3.py
+++ add_to_s3.py
@@ -17,6 +17,14 @@ def add_xml_for_s3(vol_name):
                       path_in_bucket=path_in_bucket,
                       authentication='Protected')
 
+    print("In order to add the data to the EMBL S3, please run the following command:")
+    full_s3_path = f'embl/{bucket_name}/{path_in_bucket}'
+    mc_command = f"mc cp -r {os.path.relpath(data_path)}/ {full_s3_path}/"
+    print(mc_command)
+
 
 if __name__ == '__main__':
-    add_xml_for_s3('raw')
+    # add_xml_for_s3('raw')
+    add_xml_for_s3('boutons')
+    add_xml_for_s3('mask')
+    add_xml_for_s3('traces')
