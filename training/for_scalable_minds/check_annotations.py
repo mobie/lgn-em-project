799e27dbde65e7a2d3a318268629e8aef0bc3daa
Constantin Pape <constantin.pape@embl.de>
2021-09-09 12:59:16 +0200

    Update training data for neuron seg

--- training/for_scalable_minds/check_annotations.py
+++ training/for_scalable_minds/check_annotations.py
@@ -1,9 +1,23 @@
 import argparse
+import os
 import napari
 from elf.io import open_file
 
+ANNOTATION_PATHS = [
+    "./annotations_v2/2021-08-237_13-55__training_data_z465y15673x20488-z529y16185x21000__volume",
+    "./annotations_v2/2021-08-237_13-55__training_data_z481y17612x23043-z545y18124x23555__volume",
+    "./annotations_v2/2021-08-237_13-56__training_data_z481y16967x21745-z545y17479x22257__volume",
+    "./annotations_v2/2021-08-237_13-56__training_data_z485y17596x23654-z549y18108x24166__volume",
+    "./annotations_v2/2021-08-238_16-24__training_data_z364y13744x11342-z428y14256x11854__volume",
+    "./annotations_v2/2021-09-250_09-55__training_data_z364y13905x13264-z428y14417x13776__volume",
+    "./annotations_v1/annotations2/2021-08-222_13-35__training_data_z1235y13855x14657-z1299y14367x15169__volume",
+    "./annotations_v3/2021-08-222_13-35__training_data_z1235y13855x14657-z1299y14367x15169__volume",
+    "./annotations_v3/2021-09-251_08-28__training_data_z1248y14877x14589-z1312y15389x15101__volume",
+    "./annotations_v3/2021-09-244_09-17__training_data_z1250y11863x13631-z1314y12375x14143__volume",
+]
 
-def check_annotations(raw_path, annotation_path):
+
+def check_annotations(raw_path, annotation_path, block_id):
     with open_file(raw_path, "r") as f:
         raw = f["raw"][:]
     with open_file(annotation_path, "r") as f:
@@ -12,40 +26,38 @@ def check_annotations(raw_path, annotation_path):
 
     with napari.gui_qt():
         v = napari.Viewer()
+        v.title = f"Block-{block_id}"
         v.add_image(raw)
         v.add_labels(seg)
 
 
 def check_block(block_id):
-    raw_paths = {
-        1: "./training_data/training_data_z481y16967x21745-z545y17479x22257.h5",
-        2: "./training_data/training_data_z364y13744x11342-z428y14256x11854.h5",
-        3: "./training_data/training_data_z485y17596x23654-z549y18108x24166.h5",
-        4: "./training_data2/training_data_z1235y13855x14657-z1299y14367x15169.h5",
-        11: "./training_data2/training_data_z1235y13855x14657-z1299y14367x15169.h5",
-        12: "./training_data2/training_data_z1248y14877x14589-z1312y15389x15101.h5",
-        13: "./training_data2/training_data_z1250y11863x13631-z1314y12375x14143.h5",
-    }
-    annotation_paths = {
-        1: "./annotations_v1/annotations/2021-08-222_09-24__training_data_z481y16967x21745-z545y17479x22257__volume",
-        2: "./annotations_v1/annotations/2021-08-222_09-25__training_data_z364y13744x11342-z428y14256x11854__volume",
-        3: "./annotations_v1/annotations/2021-08-222_09-25__training_data_z485y17596x23654-z549y18108x24166__volume",
-        4: "./annotations_v1/annotations2/2021-08-222_13-35__training_data_z1235y13855x14657-z1299y14367x15169__volume",
-        5: "",
-        6: "",
-        7: "",
-        8: "",
-        9: "",
-        10: "",
-        11: "./annotations_v3/2021-08-222_13-35__training_data_z1235y13855x14657-z1299y14367x15169__volume",
-        12: "./annotations_v3/2021-08-235_08-48__training_data_z1248y14877x14589-z1312y15389x15101__volume",
-        13: "./annotations_v3/2021-09-244_09-17__training_data_z1250y11863x13631-z1314y12375x14143__volume",
-    }
-    check_annotations(raw_paths[block_id], annotation_paths[block_id])
+    print("Checking block", block_id)
+    raw_dir1 = "./training_data"
+    raw_dir2 = "./training_data2"
+    annotation_path = ANNOTATION_PATHS[block_id - 1]
+
+    vol_name = os.path.split(annotation_path)[1]
+    vol_name = vol_name.split("__")[1]
+    raw_path = os.path.join(raw_dir1, vol_name + ".h5")
+    if not os.path.exists(raw_path):
+        raw_path = os.path.join(raw_dir2, vol_name + ".h5")
+    assert os.path.exists(raw_path), f"{vol_name}: {raw_path}"
+
+    check_annotations(raw_path, annotation_path, block_id)
+
+
+def check_all():
+    for block_id in range(1, len(ANNOTATION_PATHS) + 1):
+        check_block(block_id)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("block_id", type=int)
+    parser.add_argument("--block_id", "-b", type=int, default=None)
     args = parser.parse_args()
-    check_block(args.block_id)
+    block_id = args.block_id
+    if block_id is None:
+        check_all()
+    else:
+        check_block(args.block_id)
--- training/neurons/export_training_data.py
+++ training/neurons/export_training_data.py
@@ -12,7 +12,7 @@ def postprocess_seg(seg, min_bg_size=50):
     labeled = label(seg == 0)
     labeled[labeled > 0] += label_offset
     bg_ids = np.unique(labeled)[1:]
-    seg += labeled
+    seg += labeled.astype("uint32")
     assert 0 not in seg
     rag = compute_rag(seg, n_threads=8)
 
@@ -143,5 +143,60 @@ def check_mismatched_annotations():
         v.add_labels(labs)
 
 
+def get_paths_v3():
+    annotation_paths = [
+        "../for_scalable_minds/annotations_v2/2021-08-237_13-55__training_data_z465y15673x20488-z529y16185x21000__volume",
+        "../for_scalable_minds/annotations_v2/2021-08-237_13-55__training_data_z481y17612x23043-z545y18124x23555__volume",
+        "../for_scalable_minds/annotations_v2/2021-08-237_13-56__training_data_z481y16967x21745-z545y17479x22257__volume",
+        "../for_scalable_minds/annotations_v2/2021-08-237_13-56__training_data_z485y17596x23654-z549y18108x24166__volume",
+        "../for_scalable_minds/annotations_v2/2021-08-238_16-24__training_data_z364y13744x11342-z428y14256x11854__volume",
+        "../for_scalable_minds/annotations_v2/2021-09-250_09-55__training_data_z364y13905x13264-z428y14417x13776__volume",
+        "../for_scalable_minds/annotations_v1/annotations2/2021-08-222_13-35__training_data_z1235y13855x14657-z1299y14367x15169__volume",
+        "../for_scalable_minds/annotations_v3/2021-08-222_13-35__training_data_z1235y13855x14657-z1299y14367x15169__volume",
+        "../for_scalable_minds/annotations_v3/2021-09-251_08-28__training_data_z1248y14877x14589-z1312y15389x15101__volume",
+        "../for_scalable_minds/annotations_v3/2021-09-244_09-17__training_data_z1250y11863x13631-z1314y12375x14143__volume",
+    ]
+    raw_dir1 = "../for_scalable_minds/training_data"
+    raw_dir2 = "../for_scalable_minds/training_data2"
+
+    raw_paths = []
+    for annotation_path in annotation_paths:
+        vol_name = os.path.split(annotation_path)[1]
+        vol_name = vol_name.split("__")[1]
+        raw_path = os.path.join(raw_dir1, vol_name + ".h5")
+        if not os.path.exists(raw_path):
+            raw_path = os.path.join(raw_dir2, vol_name + ".h5")
+        assert os.path.exists(raw_path), f"{vol_name}: {raw_path}"
+        raw_paths.append(raw_path)
+
+    assert len(raw_paths) == len(annotation_paths)
+    return raw_paths, annotation_paths
+
+
+def export_training_data_v3(check=True):
+    raw_paths, annotation_paths = get_paths_v3()
+    output_folder = "./training_data/v3"
+    os.makedirs(output_folder, exist_ok=True)
+
+    for ii, (rp, ap) in enumerate(zip(raw_paths, annotation_paths), 1):
+        with open_file(rp, "r") as f:
+            raw = f["raw"][:]
+        with open_file(ap, "r") as f:
+            seg = f["*.tiff"][:].astype("uint32")
+        seg = postprocess_seg(seg)
+        assert raw.shape == seg.shape
+
+        name = os.path.split(rp)[1]
+        if check:
+            check_data(raw, seg, name)
+        else:
+            out_path = os.path.join(output_folder, f"block{ii}.h5")
+            with open_file(out_path, "w") as f:
+                f.create_dataset("raw", data=raw, compression="gzip")
+                f.create_dataset("labels", data=seg, compression="gzip")
+                f.attrs["name"] = name
+
+
+export_training_data_v3(check=False)
 # export_training_data_v2(check=False)
-check_mismatched_annotations()
+# check_mismatched_annotations()
